dc59d1395de4eb89d9bdf088fc37ac44e2a2554b
anton <anton@example.com>
2022-03-11 21:49:24 +0300

    3D

---
+++ src/com/github/anton_sinelnik/threeDigits.java
@@ -0,0 +1,28 @@
+package com.github.anton_sinelnik;
+
+import java.util.Scanner;
+
+/** Ввести с клавиатуры три целых числа. Определить, есть ли среди них хотя бы одна пара равных между собой чисел.
+ *  Если такая пара существует, вывести на экран числа через пробел. Если все три числа равны между собой,
+ *  то вывести все три.
+ */
+
+public class threeDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введи три числа");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b  && b!=c) {
+            System.out.println(a + " " + b);
+        } else if (b == c  && c!=a) {
+            System.out.println(b + " " + c);
+        } else if (a == c  && b!=c) {
+            System.out.println(a + " " + c);
+        } else if (a == b && b == c) {
+            System.out.println(a + " " + b + " " + c);
+        }
+    }
+}
+
