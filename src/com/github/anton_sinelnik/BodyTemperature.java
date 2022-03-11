d1b851ba7cfb882f11f126121a6ddb983b0c6930
anton <anton@example.com>
2022-03-11 21:28:48 +0300

    BT

--- src/com/github/anton_sinelnik/BodyTemperature.java
+++ src/com/github/anton_sinelnik/BodyTemperature.java
@@ -1,7 +1,30 @@
 package com.github.anton_sinelnik;
 
-public class BodyTemperature {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+/** Программа считывает с клавиатуры значение температуры тела и выдает сообщение о том,
+ * что температура тела высокая, низкая или нормальная, в зависимости от условий.
+ * В классе объявлены две булевые переменные isHigh (высокая температура) и isLow (низкая),
+ * в которые нужно вынести соответствующие условия и вместо выражений сравнения использовать
+ * эти переменные.
+ */
+
+public class BodyTemperature {
+    private static boolean isHigh;
+    private static boolean isLow;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите значение температуры");
+        double bodyTemperature = scanner.nextDouble();
+        isHigh = bodyTemperature>37;
+        isLow = bodyTemperature<36;
+        if (isHigh) {
+            System.out.println("высокая");
+        } else if (isLow) {
+            System.out.println("низкая");
+        } else {
+            System.out.println("нормальная");
+        }
+}
 }
