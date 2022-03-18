cff76254825075377af01026bffab6d9d8049786
anton <anton@example.com>
2022-03-18 13:23:32 +0300

    from8

---
+++ src/com/github/anton_sinelnik/TriangleFromEight.java
@@ -0,0 +1,12 @@
+package com.github.anton_sinelnik;
+
+public class TriangleFromEight {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+            System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}
